f02612f1f29132f86027f93fe6cf00e57423d0e5
Michal Strbka <michal.strbka@zuehlke.com>
2021-09-28 21:47:31 +0200

    static pdfgenerator

--- Source/DocxToPdf/DocxToPdf/PdfGenerator.cs
+++ Source/DocxToPdf/DocxToPdf/PdfGenerator.cs
@@ -1,6 +1,12 @@
-﻿namespace Proxoft.DocxToPdf
+﻿using System.Text;
+
+namespace Proxoft.DocxToPdf
 {
     public class PdfGenerator
     {
+        static PdfGenerator()
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        }
     }
 }
