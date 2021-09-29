0ec9c914cc14e339b73846e787337f79a327e662
Michal Strbka <michal.strbka@zuehlke.com>
2021-09-29 10:16:50 +0200

    Footer tests

---
+++ Source/DocxToPdf.Tests/FooterTests.cs
@@ -0,0 +1,43 @@
+﻿using Xunit;
+
+namespace Proxoft.DocxToPdf.Tests
+{
+    public class FooterTests : TestBase
+    {
+        public FooterTests() : base("Footers")
+        {
+            this.Options = RenderingOptions.WithDefaults(
+                footer: true);
+        }
+
+        [Fact]
+        public void HelloWorld()
+        {
+            this.Generate(nameof(HelloWorld));
+        }
+
+        [Fact]
+        public void Default()
+        {
+            this.Generate(nameof(Default));
+        }
+
+        [Fact]
+        public void EvenOdd()
+        {
+            this.Generate(nameof(EvenOdd));
+        }
+
+        [Fact]
+        public void FirstEvenOdd()
+        {
+            this.Generate(nameof(FirstEvenOdd));
+        }
+
+        [Fact]
+        public void FirstEvenOddXL()
+        {
+            this.Generate(nameof(FirstEvenOddXL));
+        }
+    }
+}
