859b0c13938c275a4ea8a2579adafbb79523e23c
Michal Strbka <michal.strbka@zuehlke.com>
2021-09-29 10:21:31 +0200

    PageNumbers tests

---
+++ Source/DocxToPdf.Tests/PageNumberTests.cs
@@ -0,0 +1,41 @@
+﻿using Xunit;
+
+namespace Proxoft.DocxToPdf.Tests
+{
+    public class PageNumberTests : TestBase
+    {
+        public PageNumberTests() : base("PageNumbers")
+        {
+        }
+
+        [Fact]
+        public void PageNumber()
+        {
+            this.Generate(nameof(PageNumber));
+        }
+
+        [Fact]
+        public void PageNumberTotalPages()
+        {
+            this.Generate(nameof(PageNumberTotalPages));
+        }
+
+        [Fact]
+        public void PageNumberTotalPages_Over10()
+        {
+            this.Generate(nameof(PageNumberTotalPages_Over10));
+        }
+
+        [Fact]
+        public void TotalPages_ReconstructParagraph()
+        {
+            this.Generate(nameof(TotalPages_ReconstructParagraph));
+        }
+
+        [Fact]
+        public void TotalPages_ReconstructMultipleParagraphs()
+        {
+            this.Generate(nameof(TotalPages_ReconstructMultipleParagraphs));
+        }
+    }
+}
