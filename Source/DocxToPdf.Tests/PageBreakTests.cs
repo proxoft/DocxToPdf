cfcbcfd37ce1f367a5bfa31c16c9f8559e102c25
Michal Strbka <michal.strbka@zuehlke.com>
2021-09-29 09:44:24 +0200

    page break tests

---
+++ Source/DocxToPdf.Tests/PageBreakTests.cs
@@ -0,0 +1,22 @@
+﻿
+using Proxoft.DocxToPdf;
+using Xunit;
+
+namespace Sidea.DocxToPdf.Tests
+{
+    public class PageBreakTests : TestBase
+    {
+        public PageBreakTests() : base("PageBreaks")
+        {
+            this.Options = new RenderingOptions(
+                hiddenChars: true,
+                paragraphBorders: RenderingOptions.ParagraphDefault);
+        }
+
+        [Fact]
+        public void PageBreak()
+        {
+            this.Generate(nameof(PageBreak));
+        }
+    }
+}
