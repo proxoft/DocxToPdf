a0cb3ed61f58ad248c9da018e2d093540eee566c
Michal Strbka <Michal.Strbka@zuehlke.com>
2025-07-10 15:13:52 +0200

    nugets update

--- Source/DocxToPdf.Tests/HeaderTest.cs
+++ Source/DocxToPdf.Tests/HeaderTest.cs
@@ -1,48 +1,45 @@
-﻿using Xunit;
+﻿namespace Proxoft.DocxToPdf.Tests;
 
-namespace Proxoft.DocxToPdf.Tests
+public class HeaderTest : TestBase
 {
-    public class HeaderTest : TestBase
+    public HeaderTest() : base("Headers")
     {
-        public HeaderTest() : base("Headers")
-        {
-            this.Options = RenderingOptions.WithDefaults(header: true);
-        }
+        this.Options = RenderingOptions.WithDefaults(header: true);
+    }
 
-        [Fact]
-        public void HelloWorld()
-        {
-            this.Generate(nameof(HelloWorld));
-        }
+    [Fact]
+    public void HelloWorld()
+    {
+        this.Generate(nameof(HelloWorld));
+    }
 
-        [Fact]
-        public void XXL()
-        {
-            this.Generate(nameof(XXL));
-        }
+    [Fact]
+    public void XXL()
+    {
+        this.Generate(nameof(XXL));
+    }
 
-        [Fact]
-        public void Default()
-        {
-            this.Generate(nameof(Default));
-        }
+    [Fact]
+    public void Default()
+    {
+        this.Generate(nameof(Default));
+    }
 
-        [Fact]
-        public void OddEven()
-        {
-            this.Generate(nameof(OddEven));
-        }
+    [Fact]
+    public void OddEven()
+    {
+        this.Generate(nameof(OddEven));
+    }
 
-        [Fact]
-        public void FirstEvenOddEvenOdd()
-        {
-            this.Generate(nameof(FirstEvenOddEvenOdd));
-        }
+    [Fact]
+    public void FirstEvenOddEvenOdd()
+    {
+        this.Generate(nameof(FirstEvenOddEvenOdd));
+    }
 
-        [Fact]
-        public void FirstEvenOddXXL()
-        {
-            this.Generate(nameof(FirstEvenOddXXL));
-        }
+    [Fact]
+    public void FirstEvenOddXXL()
+    {
+        this.Generate(nameof(FirstEvenOddXXL));
     }
 }
