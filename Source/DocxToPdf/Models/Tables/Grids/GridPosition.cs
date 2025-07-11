e4e158dbdc419da5cdfee4d61c4dfed06b0435c4
Michal Strbka <Michal.Strbka@zuehlke.com>
2025-07-11 10:08:03 +0200

    removed commented code

--- Source/DocxToPdf/Models/Tables/Grids/GridPosition.cs
+++ Source/DocxToPdf/Models/Tables/Grids/GridPosition.cs
@@ -1,61 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 
-namespace Proxoft.DocxToPdf.Models.Tables.Grids
+namespace Proxoft.DocxToPdf.Models.Tables.Grids;
+
+[DebuggerDisplay("{Column}({ColumnSpan})-{Row}({RowSpan})")]
+internal class GridPosition
 {
-    [DebuggerDisplay("{Column}({ColumnSpan})-{Row}({RowSpan})")]
-    internal class GridPosition
+    private readonly int[] _rowIndeces;
+
+    public GridPosition(
+        int column,
+        int columnSpan,
+        int row,
+        int rowSpan)
     {
-        private readonly int[] _rowIndeces;
+        this.Row = row;
+        this.Column = column;
+        this.RowSpan = rowSpan;
+        this.ColumnSpan = columnSpan;
 
-        public GridPosition(
-            int column,
-            int columnSpan,
-            int row,
-            int rowSpan)
-        {
-            this.Row = row;
-            this.Column = column;
-            this.RowSpan = rowSpan;
-            this.ColumnSpan = columnSpan;
-
-            _rowIndeces = rowSpan > 0
-                ? [.. Enumerable.Range(row, rowSpan)]
-                : [];
-        }
-
-        public int Column { get; }
-        public int ColumnSpan { get; }
-        public int Row { get; }
-        public int RowSpan { get; }
-        public bool IsFirstVerticalCell => this.RowSpan > 0;
-        public bool IsLastVerticalCell { get; }
-
-        public IReadOnlyCollection<int> RowIndeces => _rowIndeces;
-
-        public bool IsRowMergedCell => this.RowSpan <= 0;
-
-        //public bool IsInRow(int rowIndex)
-        //{
-        //    return this.Row == rowIndex || _rowIndeces.Contains(rowIndex);
-        //}
-
-        //public bool IsInColumn(int column)
-        //{
-        //    return this.Column <= column
-        //        && this.Column + this.ColumnSpan - 1 >= column;
-        //}
-
-        //public bool IsFirstVerticalCellOfRow(int rowIndex)
-        //{
-        //    return this.Row == rowIndex && this.IsFirstVerticalCell;
-        //}
-
-        //public bool IsLastVerticalCellOfRow(int rowIndex)
-        //{
-        //    return this.Row == rowIndex && this.IsLastVerticalCell;
-        //}
+        _rowIndeces = rowSpan > 0
+            ? [.. Enumerable.Range(row, rowSpan)]
+            : [];
     }
+
+    public int Column { get; }
+    public int ColumnSpan { get; }
+    public int Row { get; }
+    public int RowSpan { get; }
+    public bool IsFirstVerticalCell => this.RowSpan > 0;
+    public bool IsLastVerticalCell { get; }
+
+    public IReadOnlyCollection<int> RowIndeces => _rowIndeces;
+
+    public bool IsRowMergedCell => this.RowSpan <= 0;
 }
