867afc2f39f30208c933bdd806e21b1b07fa9e2e
Michal Strbka <Michal.Strbka@zuehlke.com>
2025-08-11 09:53:15 +0200

    DP-18: PageBreakTest

---
+++ Source/DocxToPdf.Tests/PageBreakV2Test.cs
@@ -0,0 +1,29 @@
+﻿using Proxoft.DocxToPdf.Tests.Tools;
+using Proxoft.DocxToPdf.Documents.Styles;
+using Proxoft.DocxToPdf.Documents.Styles.Borders;
+
+namespace Proxoft.DocxToPdf.Tests;
+
+public class PageBreakV2Test
+{
+    private readonly DocxToPdfExecutor _executor = new(
+        "PageBreaks/{0}.docx",
+        "PageBreaks/{0}_v2.pdf",
+        new()
+        {
+            ParagraphBorder = new BorderStyle(new Color("FFA500"), 1, LineStyle.Solid),
+            RenderParagraphCharacter = true,
+        }
+    );
+
+    [Fact]
+    public void PageBreak()
+    {
+        _executor.Convert("PageBreak", pages =>
+        {
+            pages
+                .Should()
+                .NotBeEmpty();
+        });
+    }
+}
