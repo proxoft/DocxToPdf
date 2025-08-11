5fd1c120ff37e889d04a2f8a602586c143e6c90e
Michal Strbka <Michal.Strbka@zuehlke.com>
2025-08-11 13:09:24 +0200

    DP-18: page numbering tests

---
+++ Source/DocxToPdf.Tests/PageNumberV2Test.cs
@@ -0,0 +1,73 @@
+﻿using Proxoft.DocxToPdf.Tests.Tools;
+using Proxoft.DocxToPdf.Documents.Styles;
+using Proxoft.DocxToPdf.Documents.Styles.Borders;
+
+namespace Proxoft.DocxToPdf.Tests;
+
+public class PageNumberV2Test
+{
+    private readonly DocxToPdfExecutor _executor = new(
+        "PageNumbers/{0}.docx",
+        "PageNumbers/{0}_v2.pdf",
+        new()
+        {
+            ParagraphBorder = new BorderStyle(new Color("FFA500"), 1, LineStyle.Solid),
+            RenderParagraphCharacter = true,
+        }
+    );
+
+    [Fact]
+    public void PageNumber()
+    {
+        _executor.Convert("PageNumber", pages =>
+        {
+            pages
+                .Should()
+                .NotBeEmpty();
+        });
+    }
+
+    [Fact]
+    public void PageNumberTotalPages()
+    {
+        _executor.Convert("PageNumberTotalPages", pages =>
+        {
+            pages
+                .Should()
+                .NotBeEmpty();
+        });
+    }
+
+    [Fact]
+    public void PageNumberTotalPagesOver10()
+    {
+        _executor.Convert("PageNumberTotalPagesOver10", pages =>
+        {
+            pages
+                .Should()
+                .NotBeEmpty();
+        });
+    }
+
+    [Fact]
+    public void TotalPagesReconstructParagraph()
+    {
+        _executor.Convert("TotalPagesReconstructParagraph", pages =>
+        {
+            pages
+                .Should()
+                .NotBeEmpty();
+        });
+    }
+
+    [Fact]
+    public void TotalPagesReconstructMultipleParagraphs()
+    {
+        _executor.Convert("TotalPagesReconstructMultipleParagraphs", pages =>
+        {
+            pages
+                .Should()
+                .NotBeEmpty();
+        });
+    }
+}
